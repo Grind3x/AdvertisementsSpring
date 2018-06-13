f9a67571973c0e46f46e607712dfb4d8b1a73882
Grind3x <grind3x@gmail.com>
2018-06-13 21:56:26 +0300

    upd

--- src/main/java/com/gmail/grind3x/util/Base64Adapter.java
+++ src/main/java/com/gmail/grind3x/util/Base64Adapter.java
@@ -1,17 +1,18 @@
 package com.gmail.grind3x.util;
 
+import org.apache.commons.codec.binary.Base64;
 import javax.xml.bind.annotation.adapters.XmlAdapter;
 
 public final class Base64Adapter extends XmlAdapter<String, byte[]> {
     public byte[] unmarshal(String s) {
         if (s == null)
             return null;
-        return org.apache.commons.codec.binary.Base64.decodeBase64(s);
+        return Base64.decodeBase64(s);
     }
 
     public String marshal(byte[] bytes) {
         if (bytes == null)
             return null;
-        return org.apache.commons.codec.binary.Base64.encodeBase64String(bytes);
+        return Base64.encodeBase64String(bytes);
     }
 }
